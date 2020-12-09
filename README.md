0b72d6932492430dd0972cacd2af57587b45d520
Ben Hong <ben@bencodezen.io>
2020-12-09 11:21:07 -0500

    feature: add ability to create a new pet

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Requirements
 
 - [x] User can see the tamagotchi wrapper on the page
-- [ ] User can initialize a new pet with a name
+- [x] User can initialize a new pet with a name
 - [ ] User can see the dinosaur in the tamagotchi
 - [ ] User can switch the pets mood manually
 - [ ] User can customize their tamagotchi colors
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,40 @@
-<template>
-  <div class="home">
-    <GigaVue />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
+import { reactive, toRefs } from "vue";
 import GigaVue from "@/components/GigaVue.vue";
 
 export default {
   name: "Home",
   components: {
     GigaVue
+  },
+  setup() {
+    const state = reactive({
+      petName: "",
+      userInput: "",
+      showCreatePetForm: true
+    });
+
+    const createPet = () => {
+      state.petName = state.userInput;
+      state.showCreatePetForm = false;
+    };
+
+    return {
+      ...toRefs(state),
+      createPet
+    };
   }
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ petName }}</h1>
+    <GigaVue />
+    <form v-if="showCreatePetForm" @submit.prevent>
+      <label for="pet-name">Pet Name</label>
+      <input type="text" id="pet-name" v-model="userInput" />
+      <button @click="createPet">New Pet</button>
+    </form>
+  </div>
+</template>
